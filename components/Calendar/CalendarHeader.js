706009bc911edf3aeea1ccc6d60d1e33efe4398d
javid shariff <javidshariff2005@gmail.com>
2025-07-07 14:26:43 +0530

    adding images in calendar

--- components/Calendar/CalendarHeader.js
+++ components/Calendar/CalendarHeader.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./CalendarHeader.css";
+import Image from "next/image";
+import CalendarImg from "@/public/images/calendar.png"
 
 // Accept props for current month/year and navigation functions
 const CalendarHeader = ({ currentMonthYear, onPrevWeek, onNextWeek }) => {
@@ -9,23 +11,13 @@ const CalendarHeader = ({ currentMonthYear, onPrevWeek, onNextWeek }) => {
         <div className="calendar-date-nav-wrapper">
           <span className="calendar-date-icon">
             {/* Calendar icon (Feather Icons style) */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-calendar"
-            >
-              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-              <line x1="16" y1="2" x2="16" y2="6"></line>
-              <line x1="8" y1="2" x2="8" y2="6"></line>
-              <line x1="3" y1="10" x2="21" y2="10"></line>
-            </svg>
+                        <Image
+                          className="d-flex"
+                          src={CalendarImg}
+                          alt="CalendarImg"
+                          width={20}
+                          height={20}
+                        />
           </span>
           {/* This span now displays the dynamic month/year from props */}
           <span className="calendar-date-text">{currentMonthYear}</span>
